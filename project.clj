30da70937295d8471b48550e8aa8ea17759368d8
Ethan Whaley <51497042+solidlikeplato@users.noreply.github.com>
2020-04-14 18:05:32 -0400

    removed unused plugin

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :plugins [[lein-heroku "0.5.3"]]
   :min-lein-version "2.0.0"
   :main echo_server.core
   :aot [echo_server.core]
