9152eb9179036923d98b5ad79a903e77e34b58f6
Ethan Whaley <51497042+solidlikeplato@users.noreply.github.com>
2020-04-20 16:14:45 -0400

    removed functionality, starting from scratch

--- src/echo_server/core.clj
+++ src/echo_server/core.clj
@@ -1,26 +1,12 @@
 (ns echo_server.core
-  (:import (java.io BufferedWriter OutputStreamWriter)
+  (:import (java.io InputStreamReader BufferedReader BufferedWriter OutputStreamWriter)
            (java.net ServerSocket))
   (:gen-class))
 
 (require '[clojure.string :as str])
 
-(defn sock-write [writer msg]
-  (let [line (str msg "\r\n")]
-    (. writer write line 0 (count line))))
+(defn write-hello-world []
+    (println "hello world!"))
 
-(defn write-hello-world [out]
-    (sock-write out (str "HTTP/1.1 200 OK\r\nContent-Type:text/html\r\nContent-Length:14\r\n\r\nhello world!!!"))
-    (.flush out))
-
-(defn server [port action]
-  (let [sock (ServerSocket. port)]
-    (loop []
-      (let [client-socket (. sock accept)
-              out (BufferedWriter. (OutputStreamWriter. (.getOutputStream client-socket)))]
-          (action out)
-          (.close client-socket)
-          (recur)))))
-
-(defn -main [port]
-  (server (Integer. port) write-hello-world))
+(defn -main []
+  (write-hello-world ))
