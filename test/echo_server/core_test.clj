c519b390faeed9d4a97161a36b79f97bfc0b5279
Ethan Whaley <51497042+solidlikeplato@users.noreply.github.com>
2020-04-21 11:48:27 -0400

    socket writer and test

--- test/echo_server/core_test.clj
+++ test/echo_server/core_test.clj
@@ -1,7 +1,18 @@
 (ns echo_server.core-test
   (:require [clojure.test :refer :all]
-            [echo_server.core :refer :all]))
+            [echo_server.core :refer :all])
+  (:import (java.io ByteArrayOutputStream BufferedWriter OutputStreamWriter)))
 
 (deftest a-test
   (testing "one equals one"
     (is (= 1 1))))
+
+(deftest writer-test
+  (testing "writer writes to output stream correctly"
+    (let [output (new ByteArrayOutputStream)
+          out (new BufferedWriter (new OutputStreamWriter output))
+          message "hello world"]
+
+      (socket-write out message)
+
+      (is (= message (.toString output))))))
