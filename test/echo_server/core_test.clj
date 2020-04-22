a936c3283f9f46088af201d486bd4eca2536178e
Ethan Whaley <51497042+solidlikeplato@users.noreply.github.com>
2020-04-22 10:39:15 -0400

    removed dummy test, added additional clarifying test

--- test/echo_server/core_test.clj
+++ test/echo_server/core_test.clj
@@ -3,16 +3,25 @@
             [echo_server.core :refer :all])
   (:import (java.io ByteArrayOutputStream BufferedWriter OutputStreamWriter)))
 
-(deftest a-test
-  (testing "one equals one"
-    (is (= 1 1))))
-
 (deftest writer-test
-  (testing "writer writes to output stream correctly"
+  (testing "writer writes to single string to stream correctly"
     (let [output (new ByteArrayOutputStream)
           out (new BufferedWriter (new OutputStreamWriter output))
           message "hello world"]
 
-      (socket-write out message)
+      (with-open [writer out]
+        (socket-write writer message))
 
-      (is (= message (.toString output))))))
+      (is (= message (.toString output)))))
+    
+    (testing "writer writes sequential strings stream correctly"
+    (let [output (new ByteArrayOutputStream)
+          out (new BufferedWriter (new OutputStreamWriter output))
+          message "hello world "
+          message2 "I'm an angry pink unicorn"]
+
+      (with-open [writer out]
+        (socket-write writer message)
+        (socket-write writer message2))
+
+      (is (= (str message message2) (.toString output))))))
